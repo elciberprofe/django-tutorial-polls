69be34f0a0007738183e9e8f017dbe413c872abc
Xavi <elciberprofessor@gmail.com>
2025-05-17 22:23:34 +0200

    Primer commit: Django Tutorial - Polls

---
+++ polls/models.py
@@ -0,0 +1,32 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+from django.contrib import admin
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+    def __str__(self):
+        return self.question_text
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    @property
+    def most_voted_choice(self):
+        top = self.choice_set.order_by("-votes").first()
+        if top and top.votes > 0:
+            return f"{top.choice_text} ({top.votes} vots)"
+        return "Sense vots"
+    
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
